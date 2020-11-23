f188b2be196c865438d22e4a01d2f1ebfcdb1515
daltrowilliam <daltrowilliam@gmail.com>
2020-11-23 17:51:09 -0300

    Projeto com rotas configuradas e dois botões na HomePage, nome dos integrantes no readme e json do postman

--- pokedex/src/components/HomePage.js
+++ pokedex/src/components/HomePage.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styled from 'styled-components';
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+
+
 
 function HomePage() {
-  return <div>PAGINA HOME</div>;
+  const history = useHistory();
+
+  const goToPokedexPage = (id) => {
+    history.push("/pokedex");
+  };
+
+  const goToDetailsPage = (id) => {
+    history.push("/detalhes");
+  };
+
+  return <div>
+  <button onClick={goToPokedexPage}>POKEDEX</button>
+  <button onClick={goToDetailsPage}>DETALHES</button>
+  </div>;
+  
 }
 
 export default HomePage;
